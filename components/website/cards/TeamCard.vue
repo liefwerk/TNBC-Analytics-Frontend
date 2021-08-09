5fc73650f12e165cab4c58aa48adda81761c64cf
liefwerk <natjageni@gmail.com>
2021-08-09 16:24:01 +0200

    Fixed structure issue in Team page

--- components/website/cards/TeamCard.vue
+++ components/website/cards/TeamCard.vue
@@ -1,10 +1,5 @@
 <template>
-<div class="mt-12 mb-20 md:mx-12">
-  <h1 class="title-h1 mb-20">Team Members</h1>
-  <div
-    v-for="(member, index) in team"
-    :key="index"
-    class="flex flex-col flex-wrap md:flex-row md:flex-nowrap justify-center card max-w-max">
+  <div class="flex flex-col flex-wrap md:flex-row md:flex-nowrap justify-center card max-w-max">
     <img :src="member.image">
     <div class="my-8 text-center flex-shrink md:text-left md:my-0 md:mx-8">
       <p class="py-1 md:py-0">{{ member.name }}</p>
@@ -14,7 +9,6 @@
     </div>
     <img class="" :src="member.qrCode">
   </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -22,41 +16,7 @@ import { Prop, Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class TeamCard extends Vue{
-  // @Prop({ required: true }) readonly products!: Product[]
-  public team: object[] = [
-    {
-      name: 'Jhon Doe',
-      role: 'UI/UX Designer',
-      github: 'jhondoe',
-      TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
-      image: 'TeamMember.svg',
-      qrCode: 'QRCode.svg'
-    },
-    {
-      name: 'Jhon Doe',
-      role: 'UI/UX Designer',
-      github: 'jhondoe',
-      TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
-      image: 'TeamMember.svg',
-      qrCode: 'QRCode.svg'
-    },
-    {
-      name: 'Jhon Doe',
-      role: 'UI/UX Designer',
-      github: 'jhondoe',
-      TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
-      image: 'TeamMember.svg',
-      qrCode: 'QRCode.svg'
-    },
-    {
-      name: 'Jhon Doe',
-      role: 'UI/UX Designer',
-      github: 'jhondoe',
-      TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
-      image: 'TeamMember.svg',
-      qrCode: 'QRCode.svg'
-    },
-  ]
+  @Prop({ required: true }) readonly member!: object
   
 }
 </script>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <TeamCard />
+  <div class="mt-12 mb-20 md:mx-12">
+    <h1 class="title-h1 mb-20">Team Members</h1>
+    <div
+      v-for="(member, index) in team"
+      :key="index">
+      <TeamCard :member="member" />
+    </div>
   </div>
 </template>
 
@@ -14,6 +19,44 @@ export default Vue.extend({
   components: {
     TeamCard
   },
+  data() {
+    return {
+      team: [
+        {
+          name: 'Jhon Doe',
+          role: 'UI/UX Designer',
+          github: 'jhondoe',
+          TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
+          image: 'TeamMember.svg',
+          qrCode: 'QRCode.svg'
+        },
+        {
+          name: 'Jhon Doe',
+          role: 'UI/UX Designer',
+          github: 'jhondoe',
+          TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
+          image: 'TeamMember.svg',
+          qrCode: 'QRCode.svg'
+        },
+        {
+          name: 'Jhon Doe',
+          role: 'UI/UX Designer',
+          github: 'jhondoe',
+          TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
+          image: 'TeamMember.svg',
+          qrCode: 'QRCode.svg'
+        },
+        {
+          name: 'Jhon Doe',
+          role: 'UI/UX Designer',
+          github: 'jhondoe',
+          TNBCAddress: 'a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0',
+          image: 'TeamMember.svg',
+          qrCode: 'QRCode.svg'
+        },
+      ]
+    }
+  }
 
 })
 </script>
