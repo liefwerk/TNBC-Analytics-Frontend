3f9c2fab333a6ef5187159942986a0f8e313da26
SazidAhmed <sazidahmed.official@gmail.com>
2021-08-11 22:39:55 +0600

    spinner added

--- components/website/cards/FaqCard.vue
+++ components/website/cards/FaqCard.vue
@@ -1,9 +1,12 @@
 <template>
 <div>
-    <div class="card shadow-3xl mt-5"  v-for="(faq, index) in faqs" :key="index">
-        <p class="font-medium text-primary">{{ faq.question }}</p>
-        <p class="text-secondary">{{ faq.answer }}</p>
+  <div v-for="(faq, index) in faqs" :key="index" class="relative flex flex-col flex-wrap md:flex-row md:flex-nowrap justify-center card max-w-max mx-8 md:mx-auto my-8" >
+    <div class="flex flex-col text-left flex-shrink md:text-left my-4 md:my-0 md:mx-8 self-center">
+      <p class="break-all py-2 md:py-2 font-medium text-primary">{{ faq.question }}</p>
+      <p class="break-all py-2 md:py-2 text-secondary">{{ faq.answer }}</p>
     </div>
+    <img src="upicon.svg" class="absolute top-0 right-0 pt-8 pr-8">
+  </div>
 </div>
 </template>
 
--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -1,6 +1,5 @@
 <template>
  <div class="fcprimary">
-  <!----fist freequnty---->
     <div class="px-5 py-4 mx-auto">
         <div class="flex flex-col text-center w-full mb-8">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Frequently Asked Questions</h1>
@@ -13,45 +12,64 @@
             <button class="btnclass bg-btnprimary text-btntxtclr" >Core team</button>
         </div>
     </div>
-    <!----new card line----->
-    <FaqCard :faqs="faqs"/>
+    <div  v-if="faqs.length">
+        <FaqCard :faqs="faqs"/>
+    </div>
+    <div v-else>
+        <Spinner />
+    </div>
+    
 </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import FaqCard from '@/components/website/cards/FaqCard.vue'
+import Spinner from '@/components/website/spinner/Spinner.vue'
   
 export default Vue.extend({
 
     components: {
-        FaqCard
+        FaqCard,
+        Spinner
     },
       data() {
         return {
         showup: true,
         close: false,
         faqs: [
-        {
-            id: 1,
-            question: 'What is TNBC ?',
-            answer: 'TNBC is a crypto currency.',
-            type:"treasury"
-        },
-        {
-            id: 2,
-            question: 'What is Vue ?',
-            answer: 'Vue is a progressive js framework.',
-            type:"project"
-        },
-        {
-            id: 3,
-            question: 'What is Nuxt ?',
-            answer: 'Nuxt is a framework of vue.',
-            type:"coreteam"
-        },
-      ]
-    }    
+            {
+                id: 1,
+                type:"treasury",
+                question: 'What is the current balance of Treasury ?',
+                answer: 'The current balance of Treasury is about 300,000,000 ,',
+            },
+            {
+                id: 2,
+                type:"government",
+                question: 'How much TNBC government withdrawed in goverment wallet ?',
+                answer: 'The total govt withdrawal is 50,000,000 .',
+            },
+            {
+                id: 3,
+                type:"project",
+                question: 'How much TNBC has been paid to the projects ?',
+                answer: 'About 200,000,000 TNBC has been paid to the projects .',
+            },
+            {
+                id: 4,
+                type:"coreteam",
+                question: 'How many members are there in core team ?',
+                answer: 'There are about 20 members in core team',
+            },
+        ],
+    }  
   },
+  created(){
+  },
+  mounted () {
+  },
+  methods:{
+  }
 })
 </script>
