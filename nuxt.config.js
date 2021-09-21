200628d242803a2b207f8d1524dd73b2e6462d1a
liefwerk <natjageni@gmail.com>
2021-09-21 21:02:49 +0200

    Added mode client to plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {
-      src: "~/plugins/highcharts.js",
+      src: "~/plugins/highcharts.js", mode: 'client'
     },
   ],
 
