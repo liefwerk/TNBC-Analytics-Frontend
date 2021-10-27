4a2ed74c868ca34edfbd490474a324db233c2f88
liefwerk <natjageni@gmail.com>
2021-10-27 11:28:06 +0200

    Removed dist from gitignore

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/TNBC-Analytics-Frontend/'
-  },
-
   loading: false,
 
   generate: {
