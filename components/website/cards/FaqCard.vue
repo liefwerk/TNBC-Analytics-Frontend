1f556ba2ab96d51a2cfb2db329a0c4a833433d0d
SazidAhmed <sazidahmed.official@gmail.com>
2021-11-27 08:08:05 +0600

    v-html added to faq card

--- components/website/cards/FaqCard.vue
+++ components/website/cards/FaqCard.vue
@@ -4,13 +4,13 @@
       v-if="isFirst"
       class="flex flex-col text-left flex-shrink md:text-left my-4 md:my-0 self-center transition-500">
       <p class="break-all py-1 font-medium text-primary text-subtitle">{{ item.question }}</p>
-      <p :class="!isToggled ? 'hidden' : ''" class="break-all py-1 text-secondary text-intxt transition-500">{{ item.answer }}</p>
+      <p :class="!isToggled ? 'hidden' : ''" class="break-all py-1 text-secondary text-intxt transition-500" v-html="item.answer"></p>
     </div>
     <div
       v-else
       class="flex flex-col text-left flex-shrink md:text-left my-4 md:my-0 self-center transition-500">
       <p class="break-all py-1 font-medium text-primary text-subtitle">{{ item.question }}</p>
-      <p :class="!isToggled ? 'hidden' : ''" class="break-all py-1 text-secondary text-intxt transition-500">{{ item.answer }}</p>
+      <p :class="!isToggled ? 'hidden' : ''" class="break-all py-1 text-secondary text-intxt transition-500" v-html="item.answer"></p>
     </div>
     <img v-if="isToggled" src="upicon.svg" class="absolute top-0 right-0 m-2">
     <img v-else src="downicon.svg" class="absolute top-0 right-0 m-2">
